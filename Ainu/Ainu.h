abebed7b484cb211bc8acfe49a821eb985bd691b
Henrique Sasaki Yuya <es.moriturus@icloud.com>
2015-10-12 13:33:41 +0900

    Initial Commit

---
+++ Ainu/Ainu.h
@@ -0,0 +1,19 @@
+//
+//  Ainu.h
+//  Ainu
+//
+//  Created by Henrique Sasaki Yuya on 10/12/15.
+//  Copyright © 2015 Henrique Sasaki Yuya. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Ainu.
+FOUNDATION_EXPORT double AinuVersionNumber;
+
+//! Project version string for Ainu.
+FOUNDATION_EXPORT const unsigned char AinuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ainu/PublicHeader.h>
+
+
